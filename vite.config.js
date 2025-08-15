c68d46c380edc398320f08306c3b09b0093c7f39
kavisha777 <kaviyohen@gmail.com>
2025-08-15 12:06:27 +0530

    mainjsx

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/ROLO-FRONTEND' 
+  base: '/ROLO-FRONTEND/' ,
+  plugins: [react()]
+  
 })
